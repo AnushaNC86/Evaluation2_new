17508bee33c4f0d9784b5e1575213da5fde19735
AnushaNC86 <anusha.nc@robosoftin.com>
2022-10-27 18:59:07 +0530

    last

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./modal.css";
 
 const Modal = () => {
   const existedSites = localStorage.getItem("currentUser") || "[]";
@@ -43,7 +44,60 @@ const Modal = () => {
 
   return (
     <div>
-      <div className="modalBody">
+      <div className="modalItem">
+        <div className="dashBoardForm">
+          <form className="modalForm" onSubmit={siteSubmitHandler}>
+            <div className="url">
+              <div className="classLabel">URL</div>
+              <input type="text" className="inputStyle" name="url" />
+            </div>
+
+            <div className="firstLine">
+              <div className="siteName">
+                <div className="classLabel">Site Name</div>
+                <input type="text" className="inputStyle" name="siteName" />
+              </div>
+
+              <div className="selectFolder">
+                <div className="classLabel">select/Folder</div>
+                <input type="text" className="inputStyle" name="folder" />
+              </div>
+            </div>
+
+            <div className="firstLine">
+              <div className="userName">
+                <div className="classLabel">user Name</div>
+                <input type="text" className="inputStyle" name="userName" />
+              </div>
+
+              <div className="sitePassword">
+                <div className="classLabel">site password</div>
+                <input type="text" className="inputStyle" name="sitePassword" />
+                <img
+                  src={require("../../assets/icons/eye_on.png")}
+                  alt="eye"
+                  className="passwordEye"
+                />
+              </div>
+            </div>
+
+            <div className="textArea">
+              <div className="classLabel">notes</div>
+              <textarea className="inputStyle" name="notes" />
+            </div>
+
+            <div className="modalButton">
+              <button className="modalButtons button2 " type="reset">
+                Reset
+              </button>
+              <button className="modalButtons button2" type="submit">
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      {/* <div className="modalBody">
         <form className="modalForm" onSubmit={siteSubmitHandler}>
           <div className="inputField">
             <div className="url">URL</div>
@@ -83,7 +137,7 @@ const Modal = () => {
             </button>
           </div>
         </form>
-      </div>
+      </div> */}
     </div>
   );
 };
